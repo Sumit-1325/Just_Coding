d13ffb92e120d96349308dfea089437bd17bf464
Sumit-1325 <sumit13thakur124@gmail.com>
2026-03-29 19:51:45 +0530

    Fixed the build issue

--- dist/lib/prisma.js
+++ dist/lib/prisma.js
@@ -1,7 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config(); // ← load env FIRST before reading DATABASE_URL
-import { PrismaClient } from "./generated/prisma/client.js";
+import { createRequire } from "node:module";
 import { PrismaPg } from "@prisma/adapter-pg";
+const require = createRequire(import.meta.url);
+const { PrismaClient } = require("./generated/prisma");
 const connectionString = process.env.DATABASE_URL;
 if (!connectionString) {
     throw new Error("DATABASE_URL is not defined in environment variables");
--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,8 +1,11 @@
 import dotenv from "dotenv";
 dotenv.config(); // ← load env FIRST before reading DATABASE_URL
 
-import { PrismaClient } from "./generated/prisma/client.js";
+import { createRequire } from "node:module";
 import { PrismaPg } from "@prisma/adapter-pg";
+
+const require = createRequire(import.meta.url);
+const { PrismaClient } = require("./generated/prisma");
 
 const connectionString = process.env.DATABASE_URL;
 
